d6cb368969d8b3c46fd34309779b3402bbc6ceb9
Allenjonesing <Allenjonesing@gmail.com>
2021-06-02 10:55:50 -0600

    Lot of blood added

---
+++ Assets/brick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BzKovSoft.RagdollTemplate.Scripts.Charachter;
+
+public class brick : MonoBehaviour
+{
+    public GameObject bloodParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // When a brick has a collision, we will check to see if it's a player
+    void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject != null && collision.collider.tag == "Player")
+        {
+            ContactPoint contact = collision.contacts[0];
+            Instantiate(bloodParticle, contact.point, Quaternion.identity);
+            var ragdoll = collision.gameObject.GetComponent<BzRagdoll>();
+            if (ragdoll != null)
+            {
+                ragdoll.BulletCollisionDetected();
+            }
+        }
+    }
+}
