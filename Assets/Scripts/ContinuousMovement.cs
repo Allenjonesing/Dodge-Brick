d8447506534757a453a6e2e9195329b1011afd19
Allenjonesing <Allenjonesing@gmail.com>
2021-06-03 04:01:05 -0600

    Release

--- Assets/Scripts/ContinuousMovement.cs
+++ Assets/Scripts/ContinuousMovement.cs
@@ -15,12 +15,10 @@ public class ContinuousMovement : MonoBehaviour
     private float fallingSpeed;
     private XRRig rig;
     private Vector2 inputAxis;
-    private CharacterController character;
 
     // Start is called before the first frame update
     void Start()
     {
-        character = GetComponent<CharacterController>();
         rig = GetComponent<XRRig>();
     }
 
@@ -33,36 +31,11 @@ public class ContinuousMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        CapsuleFollowHeadset();
-
         Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
-        Vector3 direction = headYaw *  new Vector3(inputAxis.x, 0, inputAxis.y);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
 
-        character.Move(direction * Time.fixedDeltaTime * speed);
-
-        //gravity
-        bool isGrounded = CheckIfGrounded();
-        if (isGrounded)
-            fallingSpeed = 0;
-        else
-            fallingSpeed += gravity * Time.fixedDeltaTime;
-
-        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
-    }
-
-    void CapsuleFollowHeadset()
-    {
-        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
-        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
-        character.center = new Vector3(capsuleCenter.x, character.height/2 + character.skinWidth , capsuleCenter.z);
-    }
-
-    bool CheckIfGrounded()
-    {
-        //tells us if on ground
-        Vector3 rayStart = transform.TransformPoint(character.center);
-        float rayLength = character.center.y + 0.01f;
-        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
-        return hasHit;
+        // Move our position a step closer to the target.
+        float step = speed * Time.fixedDeltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, step);
     }
 }
